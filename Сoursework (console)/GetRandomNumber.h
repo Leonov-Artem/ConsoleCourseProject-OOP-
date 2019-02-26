10e6596a5ef88a5682ae1a7f7fc81061a3aba664
Leonov Artem <leonovru@yandex.ru>
2019-02-26 21:20:43 +0300

    Fill GetRandomNumber.h

--- Сoursework (console)/GetRandomNumber.h
+++ Сoursework (console)/GetRandomNumber.h
@@ -2,11 +2,12 @@
 #ifndef GETRANDOMNUMBER_H
 #define GETRANDOMNUMBER_H
 
+template <typename T>
 class GetRandomNumber
 {
 public:
 	GetRandomNumber();
-	~GetRandomNumber();
+	static T FromNormalDistribution(T min, T max);
 };
 
 #endif // !GETRANDOMNUMBER_H
