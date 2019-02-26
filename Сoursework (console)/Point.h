4c27937ef9c1d0daac841472162fcae4a345e82d
Leonov Artem <leonovru@yandex.ru>
2019-02-26 21:08:03 +0300

    Fill Point.h

--- Сoursework (console)/Point.h
+++ Сoursework (console)/Point.h
@@ -2,11 +2,16 @@
 #ifndef POINT_H
 #define POINT_H
 
+template <class T>
 class Point
 {
-public:
-	Point();
-	~Point();
+	public:
+		Point();
+		Point(T x, T y);
+		T GetX();
+		T GetY();
+	private:
+		T x, y;
 };
 
 #endif // !POINT_H
