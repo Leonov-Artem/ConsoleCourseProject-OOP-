df0c7362d800fef33d03365bf87a3bbf71cd69bf
Leonov Artem <leonovru@yandex.ru>
2019-02-26 21:37:05 +0300

    Add static method GetRandomNumber

--- Сoursework (console)/GetRandomNumber.cpp
+++ Сoursework (console)/GetRandomNumber.cpp
@@ -1,1 +1,16 @@
 #include "GetRandomNumber.h"
+
+template <typename T>
+T GetRandomNumber<T>::FromNormalDistribution(T min, T max)
+{
+	T mean = (max - min) / 2;
+	T standard_deviation = (max - min) / 6;
+
+	// для генерации инициализирующего начального числа
+	std::random_device randomDevice;
+	// Вихрь Мерсенна. ГПСЧ, основанный на св-вах простых чисел Мерсенна
+	std::mt19937 randomGen(randomDevice());
+	std::normal_distribution<float> normalDistribution(mean, standard_deviation);
+
+	return normalDistribution(randomGen);
+}
