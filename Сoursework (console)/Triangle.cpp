9950f84dcf01c9f104b599a1256cf8bd9c01107a
Leonov Artem <leonovru@yandex.ru>
2019-03-05 05:07:06 +0300

    Add 'Triangle' implementation

--- Сoursework (console)/Triangle.cpp
+++ Сoursework (console)/Triangle.cpp
@@ -1,12 +1,14 @@
 #include "Triangle.h"
 
-
-
-Triangle::Triangle()
+Triangle::Triangle(Point<double> b, Point<double> d, Point<double> e)
 {
+	this->b = b;
+	this->d = d;
+	this->e = e;
 }
-
-
-Triangle::~Triangle()
+double Triangle::Area()
 {
+	double h = d.GetX() - b.GetX();
+	double a = d.GetY() - e.GetY();
+	return 0.5*a*h;
 }
--- Сoursework (console)/Triangle.h
+++ Сoursework (console)/Triangle.h
@@ -1,8 +1,14 @@
 #pragma once
+#include "Point.h"
+
 class Triangle
 {
+private:
+	Point<double> b, d, e;
+
 public:
-	Triangle();
-	~Triangle();
+	Triangle(Point<double> b, Point<double> d, Point<double> e);
+	
+	double Area();
 };
 
