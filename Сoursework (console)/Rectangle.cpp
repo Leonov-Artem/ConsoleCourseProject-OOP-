1501e0db117f1c11d7f27c639c7c2fdbecf2a127
Leonov Artem <leonovru@yandex.ru>
2019-03-06 10:50:40 +0300

    Add class implementation 'Rectangle'

--- Сoursework (console)/Rectangle.cpp
+++ Сoursework (console)/Rectangle.cpp
@@ -1,12 +1,32 @@
 #include "Rectangle.h"
+#include "Point.h"
 
-
-
-Rectangle::Rectangle()
+void Rectangle::CoordinateDetermination(Point<double> b, Point<double> d, Point<double> m, Point<double> e)
 {
+	length = m.GetX() - b.GetX();
+	width = d.GetY() - e.GetY();
+
+	A = Point<double>(b.GetX(),		e.GetY());
+	B = Point<double>(b.GetX(),		d.GetY());
+	C = Point<double>(m.GetX(),		d.GetY());
+	D = Point<double>(m.GetX(),		e.GetY());
 }
 
-
-Rectangle::~Rectangle()
-{
+Rectangle::Rectangle() 
+{ 
+	A = B = C = D = Point<double>(0, 0); 
 }
+Rectangle::Rectangle(Point<double> b, Point<double> d, Point<double> m, Point<double> e)
+{
+	CoordinateDetermination(b, d, m, e);
+}
+
+Point<double> Rectangle::GetA() { return A; }
+Point<double> Rectangle::GetB() { return B; }
+Point<double> Rectangle::GetC() { return C; }
+Point<double> Rectangle::GetD() { return D; }
+
+double Rectangle::Length()		{ return length; }
+double Rectangle::Width()		{ return width; }
+
+double Rectangle::Area()		{ return length * width; }
--- Сoursework (console)/Rectangle.h
+++ Сoursework (console)/Rectangle.h
@@ -1,8 +1,26 @@
 #pragma once
+#include "Point.h"
+
 class Rectangle
 {
+private:
+	Point<double> A, B, C, D;
+	double length, width;
+
+	void CoordinateDetermination(Point<double> b, Point<double> d, Point<double> m, Point<double> e);
+
 public:
 	Rectangle();
-	~Rectangle();
+	Rectangle(Point<double> b, Point<double> d, Point<double> m, Point<double> e);
+
+	Point<double> GetA();
+	Point<double> GetB();
+	Point<double> GetC();
+	Point<double> GetD();
+
+	double Length();
+	double Width();
+
+	double Area();
 };
 
