0a6fde91861d9e0a76f6f1057076cd68966f4deb
Leonov Artem <leonovru@yandex.ru>
2019-03-06 10:58:59 +0300

    Add new functionality in 'Triangle'

--- Сoursework (console)/Triangle.cpp
+++ Сoursework (console)/Triangle.cpp
@@ -2,13 +2,19 @@
 
 Triangle::Triangle(Point<double> b, Point<double> d, Point<double> e)
 {
+	height = d.GetX() - b.GetX();
+	base = d.GetY() - e.GetY();
+
 	this->b = b;
 	this->d = d;
 	this->e = e;
 }
-double Triangle::Area()
-{
-	double h = d.GetX() - b.GetX();
-	double a = d.GetY() - e.GetY();
-	return 0.5*a*h;
-}
+
+Point<double> Triangle::GetB()	{ return b; }
+Point<double> Triangle::GetD()	{ return d; }
+Point<double> Triangle::GetE()	{ return e; }
+
+double Triangle::Height()		{ return height; }
+double Triangle::Base()			{ return base; }
+
+double Triangle::Area()			{ return 0.5*base*height; }
--- Сoursework (console)/Triangle.h
+++ Сoursework (console)/Triangle.h
@@ -5,10 +5,17 @@ class Triangle
 {
 private:
 	Point<double> b, d, e;
+	double height, base;
 
 public:
 	Triangle(Point<double> b, Point<double> d, Point<double> e);
 	
+	Point<double> GetB();
+	Point<double> GetD();
+	Point<double> GetE();
+
+	double Height();
+	double Base();
+
 	double Area();
 };
-
