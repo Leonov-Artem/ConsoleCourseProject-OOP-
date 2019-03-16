d86f60f350c47dc1c56af35c27ada1e16b8a1a1c
Leonov Artem <leonovru@yandex.ru>
2019-03-16 10:41:48 +0300

    Add copy constructor in Vector.h

--- Сoursework (console)/Vector.h
+++ Сoursework (console)/Vector.h
@@ -10,14 +10,21 @@ class Vector
 private:
 	T x, y;
 
+	T GetX() { return x; }
+	T GetY() { return y; }
+
 public:
 	Vector()											{ x = 0; y = 0; }
 	Vector(T x, T y)									{ this->x = x; this->y = y; }
+	Vector(const Vector& vector)						{ x = vector.X; y = vector.Y; }
 	Vector(Point<T> p1, Point<T> p2)
 	{
-		x = p2.GetX() - p1.GetX();
-		y = p2.GetY() - p1.GetY();
+		x = p2.X - p1.X;
+		y = p2.Y - p1.Y;
 	}
+
+	_declspec(property(get = GetX)) int X;
+	_declspec(property(get = GetX)) int Y;
 
 	double Length()										{ return sqrt(x * x + y * y); }
 	static double Pseudoscalar(Vector v1, Vector v2)	{ return v1.x*v2.y - v2.x*v1.y; }
@@ -25,6 +32,6 @@ public:
 	static double AngleBetween(Vector v1, Vector v2)
 	{
 		double cos = Dot(v1, v2) / (v1.Length() * v2.Length());
-		return round((acos(cos) * 180.0 / M_PI) * 100) / 100;
+		return acos(cos) * 180.0 / M_PI;
 	}
 };
