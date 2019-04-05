7e594ef5e17cb7c2a2137d513323f4735c81a065
Leonov Artem <leonovru@yandex.ru>
2019-04-05 20:12:28 +0300

    Finish the file implementation Rectangle.h

--- Сoursework (procedural)/Main.cpp
+++ Сoursework (procedural)/Main.cpp
@@ -4,19 +4,20 @@
 #include "Vector.h"
 #include "Triangle.h"
 #include "Semicircle.h"
+#include "Rectangle.h"
 
 int main()
 {
 	using namespace std;
 
-	PointD b{ 5,2 };
+	PointD b{ 0, 0 };
+	PointD d{ 5, 2 };
 	PointD m{ 7, 0 };
-	PointD e{ 5,-2 };
+	PointD e{ 5, -2 };
 
-	Semicircle s;
-	s = CreateSemicircle(b, m, e);
-
-	cout << s.Area << endl;
+	Rectangle r;
+	r = CreateRectangle(b, d, m, e);
+	cout << r.length << endl;
 
 	system("pause");
 	return 0;
--- Сoursework (procedural)/Rectangle.h
+++ Сoursework (procedural)/Rectangle.h
@@ -1,1 +1,29 @@
 #pragma once
+#include "Point.h"
+
+struct Rectangle
+{
+	PointD A, B, C, D;
+	double length, width, Area;
+};
+
+void CoordinateDetermination(Rectangle& rectangle, PointD b, PointD d, PointD m, PointD e)
+{
+	rectangle.length = m.x - b.x;
+	rectangle.width = d.y - e.y;
+
+	rectangle.A = PointD {b.x, e.y};
+	rectangle.B = PointD {b.x, d.y};
+	rectangle.C = PointD {m.x, d.y};
+	rectangle.D = PointD {m.x, e.y};
+}
+
+void SetArea(Rectangle& rectangle) { rectangle.Area = rectangle.length * rectangle.width; }
+
+Rectangle CreateRectangle(PointD b, PointD d, PointD m, PointD e)
+{
+	Rectangle rectangle;
+	CoordinateDetermination(rectangle, b, d, m, e);
+	SetArea(rectangle);
+	return rectangle;
+}
