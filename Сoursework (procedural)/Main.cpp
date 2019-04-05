33a72d284cf7cad16270e0ada35720dacd0c3583
Leonov Artem <leonovru@yandex.ru>
2019-04-05 19:51:57 +0300

    Finish the file implementation Semicircle.h

--- Сoursework (procedural)/Main.cpp
+++ Сoursework (procedural)/Main.cpp
@@ -3,39 +3,20 @@
 #include "Point.h"
 #include "Vector.h"
 #include "Triangle.h"
+#include "Semicircle.h"
 
 int main()
 {
 	using namespace std;
 
-	//PointD p1{ 3, 4 };
-	//PointD p2{ 2, 8 };
-	//PointD p3{ -2, 1 };
-	//PointD p4{ 6, -12 };
+	PointD b{ 5,2 };
+	PointD m{ 7, 0 };
+	PointD e{ 5,-2 };
 
-	//Vector v1;
-	//v1 = CreateVector(p1, p2);
-	//
-	//cout << "v1.x = " << v1.x << endl;
-	//cout << "v1.y = " << v1.y << endl;
-	//cout << endl;
+	Semicircle s;
+	s = CreateSemicircle(b, m, e);
 
-	//Vector v2;
-	//v2 = CreateVector(p3, p4);
-
-	//cout << "v2.x = " << v2.x << endl;
-	//cout << "v2.y = " << v2.y << endl;
-	//cout << endl;
-
-	//cout << AngleBetween(v1, v2) << endl;
-
-	PointD p1{ 0, 0 };
-	PointD p2{ 5,2 };
-	PointD p3{ 5, -2 };
-
-	Triangle t;
-	t = CreateTriangle(p1, p2, p3);
-	cout << t.Area << endl;
+	cout << s.Area << endl;
 
 	system("pause");
 	return 0;
--- Сoursework (procedural)/Semicircle.h
+++ Сoursework (procedural)/Semicircle.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "Point.h"
+
+struct Semicircle
+{
+	PointD d, m, e, Centre;
+	double Radius, Area;
+};
+
+void SetArea(Semicircle& semicircle) 
+{ 
+	semicircle.Area = 0.5 * M_PI * semicircle.Radius * semicircle.Radius;
+}
+
+Semicircle CreateSemicircle(PointD d, PointD m, PointD e)
+{
+	Semicircle semicircle;
+
+	semicircle.d = d;
+	semicircle.m = m;
+	semicircle.e = e;
+
+	semicircle.Radius = m.x - d.x;
+	semicircle.Centre = PointD{ d.x, m.y };
+	SetArea(semicircle);
+
+	return semicircle;
+}
+
+double SemicircleEquation(Semicircle semicircle, PointD point)
+{
+	double x0 = semicircle.Centre.x;
+	double y0 = semicircle.Centre.y;
+
+	return x0 + sqrt(pow(semicircle.Radius, 2) - pow(point.y - y0, 2));
+}
+bool PointInsideSemicircle(Semicircle semicircle, PointD point)
+{
+	return (bool)(point.x < SemicircleEquation(semicircle, point));
+}
