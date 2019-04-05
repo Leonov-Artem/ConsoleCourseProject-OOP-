bc7c4b028f51a7a5743561a498f70a2b9a511b95
Leonov Artem <leonovru@yandex.ru>
2019-04-05 20:57:33 +0300

    Change RandomNumber.h

--- Сoursework (procedural)/RandomNumber.h
+++ Сoursework (procedural)/RandomNumber.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include "Point.h"
 
 double GetRandNum(double min, double max)
 {
@@ -7,4 +8,12 @@ double GetRandNum(double min, double max)
 	std::mt19937          generator(rand_dev());
 	std::uniform_real_distribution<double>  distribution(min, max);
 	return distribution(generator);
+}
+
+PointD GeneratePoint(double x_min, double x_max, double y_min, double y_max)
+{
+	double x = GetRandNum(x_min, x_max);
+	double y = GetRandNum(y_min, y_max);
+	PointD p{ x, y };
+	return p;
 }
